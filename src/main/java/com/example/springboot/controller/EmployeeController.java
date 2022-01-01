fe165a9576b36a70a53d88c6f915289ef5e68607
Osc d3w.k <oscar2019seoul@gmail.com>
2021-12-31 23:29:39 -0800

    + EmployeeController

---
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.example.springboot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.model.Employee;
+import com.example.springboot.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	// build create employee REST API
+	@PostMapping()
+	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+	}
+	
+	
+
+}
