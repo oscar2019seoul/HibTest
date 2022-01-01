4127c2e5c867989f37170115f6acd98ee948af7b
Osc d3w.k <oscar2019seoul@gmail.com>
2021-12-31 23:20:58 -0800

    + EmployeeService

---
+++ src/main/java/com/example/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.springboot.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.springboot.model.Employee;
+import com.example.springboot.repository.EmployeeRepository;
+import com.example.springboot.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+	private EmployeeRepository employeeRepository;
+
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		super();
+		this.employeeRepository = employeeRepository;
+	}
+
+
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return employeeRepository.save(employee);
+	}
+
+}
